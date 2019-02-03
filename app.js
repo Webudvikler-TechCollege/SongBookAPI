190826e45619324aaaac3f701c2b7ba37eb85289
Heinz K <heka@techcollege.dk>
2019-02-03 21:10:08 +0100

    MySql Config Module

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+      app = express(),
+      mysql = require('./config/mysql');
+
 
 const songRouter = require('./routes/songRoutes');
 const port = process.env.PORT || 4000;
 
-app.use('/api/songs', songRouter); 
+app.route('/api/songs')
+    .get((req, res) => {
+        let sql = 'SELECT * FROM song';
+        mysql.query(sql, (err, result, fields) => {
+            res.json(result);
+        });
+    });
+
+app.route('/api/song/:id')
+    .get((req, res) => {
+        let sql = 'SELECT * FROM song WHERE id = ' + req.params.id;
+        mysql.query(sql, (err, result, fields) => {
+            res.json(result);
+        });
+    })
+    .post((req, res) => {
+        res.json('query' + req.query)
+    })
+    .put((req, res) => {
+        res.json('query' + req.query)
+    })
+    .patch((req, res) => {
+        res.json('query' + req.query)
+    })
+    .delete((req, res) => {
+        res.json('query' + req.query)
+    })
+
 
 app.listen(port, () => {
     console.log('App is running...');
--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const routes = () => {
     const songRouter = express.Router();
-
+    
     songRouter.route('/')
         .get((req, res) => {
-            res.send("Hello");
+            console.log(12121);
+            res.status(201).send("Hello");
         });
 
         return songRouter;
